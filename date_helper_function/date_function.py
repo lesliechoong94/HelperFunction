89faab472d2b19740e1f6bd9ab45f17d70c22602
Leslie Wee Nam Choong <leslie.choong@grabtaxi.com>
2019-10-10 00:02:23 +0800

    added date list function

--- date_helper_function/date_function.py
+++ date_helper_function/date_function.py
@@ -2,4 +2,14 @@ from datetime import datetime
 from datetime import timedelta
 
 def add_day_to_date_string(date, days=1):
-    return((datetime.strptime(date, '%Y-%m-%d') + timedelta(days = 1)).strftime('%Y-%m-%d'))
+    """
+        String -> String
+    """
+    return((datetime.strptime(date, '%Y-%m-%d') + timedelta(days = days)).strftime('%Y-%m-%d'))
+
+def generate_date_string_range(start_date, end_date):
+    """
+        list of date string 
+        return [start_date, ... , end_date]
+    """
+    return([add_day_to_date_string(start_date, day) for day in range(0,delta.days + 1)])
